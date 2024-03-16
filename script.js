c3ef1f8c281ddb26d49acd1237187fcf6db6fec5
saotomem21 <87118475+saotomem21@users.noreply.github.com>
2024-03-16 16:43:22 +0900

    add graph
    グラフを追加

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById('next-pokemon').addEventListener('click', function() {
     changePokemon(1);
 });
 
-let currentPokemonId = 0; // 現在表示されているポケモンのIDを保持する変数
+let currentPokemonId = 0;
 
 function searchPokemon(query) {
     fetch(`https://pokeapi.co/api/v2/pokemon/${query.toLowerCase()}`)
@@ -22,18 +22,19 @@ function searchPokemon(query) {
             return response.json();
         })
         .then(data => {
-            currentPokemonId = data.id; // 現在のポケモンIDを更新
+            currentPokemonId = data.id;
             displayPokemon(data);
+            updateChart(data);
         })
         .catch(error => {
             console.error('Error:', error);
-            displayError(error.message); // エラーメッセージを表示
+            displayError(error.message);
         });
 }
 
 function changePokemon(change) {
     let newId = currentPokemonId + change;
-    if (newId < 1) newId = 1; // 図鑑番号が1未満にならないようにする
+    if (newId < 1) newId = 1;
     searchPokemon(newId.toString());
 }
 
@@ -44,16 +45,38 @@ function displayPokemon(data) {
         <img src="${data.sprites.front_default}" alt="${data.name}">
         <p>図鑑番号: ${data.id}</p>
         <p>タイプ: ${data.types.map(type => type.type.name).join(', ')}</p>
-        <p>HP: ${data.stats[0].base_stat}</p>
-        <p>攻撃: ${data.stats[1].base_stat}</p>
-        <p>防御: ${data.stats[2].base_stat}</p>
-        <p>特攻: ${data.stats[3].base_stat}</p>
-        <p>特防: ${data.stats[4].base_stat}</p>
-        <p>素早さ: ${data.stats[5].base_stat}</p>
     `;
 }
 
 function displayError(message) {
     let infoDiv = document.getElementById('pokemon-info');
     infoDiv.innerHTML = `<p>${message}</p>`;
+}
+
+function updateChart(data) {
+    let stats = data.stats.map(s => s.base_stat);
+    let ctx = document.getElementById('pokemonStatsChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'radar',
+        data: {
+            labels: ['HP', '攻撃', '防御', '特攻', '特防', '素早さ'],
+            datasets: [{
+                label: data.name,
+                data: stats,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                r: {
+                    angleLines: {
+                        display: false
+                    },
+                    suggestedMin: 0
+                }
+            }
+        }
+    });
 }
